a830376bf9ea3b88d55b9bf0bea6d87d64cc5ef7
Sujay Paul <sujaypaul531@gmail.com>
2023-01-02 21:00:47 +0530

    added more data

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,7 +1,7 @@
 export default [
     {
         id:1,
-        photo:"https://source.unsplash.com/WLxQvbMyfas",
+        photo:"https://images.unsplash.com/photo-1589308078059-be1415eab4c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNzMzOTgxMA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
         adress:{
             country:"JAPAN",
             map:"https://goo.gl/maps/nPV8gKK6NABbqju88"
@@ -15,7 +15,7 @@ export default [
     },
     {
         id:2,
-        photo:"https://source.unsplash.com/JmuyB_LibRo",
+        photo:"https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNzM0MDA3OQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
         adress:{
             country:"AUSTRALIA",
             map:"https://goo.gl/maps/Nmmb8UYzreWc8pqM7"
@@ -29,7 +29,7 @@ export default [
     },
     {
         id:3,
-        photo:"https://source.unsplash.com/3PeSjpLVtLg",
+        photo:"https://images.unsplash.com/photo-1601439678777-b2b3c56fa627?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNzM0MDA3OQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
         adress:{
             country:"NORWAY",
             map:"https://goo.gl/maps/rjV4zrj5wxmrKyDJ9"
@@ -40,5 +40,47 @@ export default [
             to:"18 Nov, 2021"
         },
         about:"The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
-    }
+    },
+    {
+        id:4,
+        photo:"https://kolkatatourism.travel/images/places-to-visit/headers/belur-math-kolkata-entry-fee-timings-holidays-reviews-header.jpg",
+        adress:{
+            country:"INDIA",
+            map:"https://goo.gl/maps/Ng5QDpamVAr1Aitz6"
+        },
+        name:"Belur Math",
+        date:{
+            from:"20 dec, 2022",
+            to:"21 dec, 2022"
+        },
+        about:"Belur Math is the international headquarters of Ramakrishna Mission. It was founded in 1899 by Swami Vivekananda, the world famous yogi and disciple of Sri Ramakrishna Paramahansa. This International tourist attraction is located near Belur in Howrah district."
+    },
+    {
+        id:5,
+        photo:"https://yometro.com/images/places/dakshineswar-kali-temple.jpg",
+        adress:{
+            country:"INDIA",
+            map:"https://goo.gl/maps/rrU67Cdc4MfuoCHc9"
+        },
+        name:"Dakshineswar Kali Temple",
+        date:{
+            from:"20 dec, 2022",
+            to:"21 dec, 2022"
+        },
+        about:"Dakshineswar Kali temple is one of the holiest places of Eastern India. It is one of the must visit places in Kolkata. Dakshineswar is a Hindu temple located in Dakshineswar near Kolkata. Situated on the eastern bank of the Hooghly River, the presiding deity of the temple is Bhavatarini."
+    },
+    {
+        id:6,
+        photo:"https://www.thestatesman.com/wp-content/uploads/2019/06/BHUTAN.jpg",
+        adress:{
+            country:"BHUTAN",
+            map:"https://goo.gl/maps/511cSQfECvRvQB9M6"
+        },
+        name:"Phuentsholing",
+        date:{
+            from:"7 sep, 2015",
+            to:"8 sep, 2015"
+        },
+        about:"The second largest town in Bhutan, Phuentsholing shares its borders with the Indian State of West Bengal. Serving as an entry point for travellers from Kolkata and Siliguri, it is an important economic hub of Bhutan. An urban centre, it is more developed than most of the towns of Bhutan, yet it has a perfect amount of nature's touch. It offers an experience like nothing else. Home to different communities, it's a must-visit place in Bhutan."
+    },
 ]
